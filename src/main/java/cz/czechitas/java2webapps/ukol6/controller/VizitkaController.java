ed7ec0df00d5c0fcccb0bbeb050ce84b9d677600
alzbetahladikova <beta.strelbova@gmail.com>
2023-11-22 17:26:42 +0100

    vytvořena VizitkaRepository

--- src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -4,7 +4,9 @@ import cz.czechitas.java2webapps.ukol6.service.VizitkaService;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class VizitkaController {
